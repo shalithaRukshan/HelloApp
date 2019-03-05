a9504e1c08f233136f8d478e70335345c3facdd2
shalitha <mdsrwijethilaka@gmail.com>
2019-03-05 02:22:36 -0800

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ class helloworld{
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("changed");
-		System.out.println("saaaaaaa");
+		System.out.println("saaaa");
 	}
 }
